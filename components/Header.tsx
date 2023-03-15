755f7e19c3f93abd1bcb858f4d34d9b03b1b4c35
Marcel Weber <mrweber@gmx.ch>
2023-03-15 20:19:53 +0100

    Added type definition

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,18 @@
 import Head from "next/head"
 
-export default function Header(props){
+export default function Header({title, content, url}:{title: string; content: string; url: string;}){
     return(
         <Head>
-            <title>{props.title}</title>
-            <meta name="description" content={props.title} />
+            <title>{title}</title>
+            <meta name="description" content={title} />
             <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:title" content={props.title} />
-            <meta name="twitter:description" content={props.content} />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:description" content={content} />
             <meta name="twitter:image" content="" />
             <meta property="og:type" content="article" />
-            <meta property="og:title" content={props.title} />
-            <meta property="og:description" content={props.content} />
-            <meta property="og:url" content={props.url} />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={content} />
+            <meta property="og:url" content={url} />
             <meta property="og:image" content="" />
             <link rel="icon" href="" />
         </Head>
