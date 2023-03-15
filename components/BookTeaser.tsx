55e4222c885577780de076697b6c3257979aa6fc
Marcel Weber <mrweber@gmx.ch>
2023-03-15 20:17:07 +0100

    Fixed class/classNAme typo

--- components/BookTeaser.tsx
+++ components/BookTeaser.tsx
@@ -3,7 +3,7 @@ import s from "@/styles/bookTeaser.module.css"
 export default function BookTeaser({book} :{book: string}){
     return(
         <div className={s.container}>
-            <div class={s.thumb} style={{backgroundImage: `url("/buch_${book}.jpg")`}}>
+            <div className={s.thumb} style={{backgroundImage: `url("/buch_${book}.jpg")`}}>
             </div>
             <button className={s.button}>
                 {
