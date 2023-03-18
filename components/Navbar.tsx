a042e7e08bb4f66865af63ffd39c2f055360e20f
Marcel Weber <mrweber@gmx.ch>
2023-03-18 13:25:55 +0100

    Typo in Spezialangebote

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,28 @@
-import { useState, useEffect } from "react"
-import Link from "next/link"
-import s from "@/styles/navbar.module.css"
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import s from "@/styles/navbar.module.css";
 
-export default function Navbar(){
-
-    return(
-        <nav className ={s.nav}>
-            <div className={s.linkContainer}>
-                <Link className={s.link} href="/">Home</Link>
-                <Link className={s.link} href="/drucksachen">Drucksachen</Link>
-                <Link className={s.link} href="/speziealangebote">Spezialangebote</Link>
-                <Link className={s.link} href="/buecher">Bücher</Link>
-                <Link className={s.link} href="/kontakt">Kontakt</Link>
-                <Link className={s.homeLink} href="/"></Link>
-            </div>
-        </nav>
-    )
+export default function Navbar() {
+  return (
+    <nav className={s.nav}>
+      <div className={s.linkContainer}>
+        <Link className={s.link} href="/">
+          Home
+        </Link>
+        <Link className={s.link} href="/drucksachen">
+          Drucksachen
+        </Link>
+        <Link className={s.link} href="/spezialangebote">
+          Spezialangebote
+        </Link>
+        <Link className={s.link} href="/buecher">
+          Bücher
+        </Link>
+        <Link className={s.link} href="/kontakt">
+          Kontakt
+        </Link>
+        <Link className={s.homeLink} href="/"></Link>
+      </div>
+    </nav>
+  );
 }
