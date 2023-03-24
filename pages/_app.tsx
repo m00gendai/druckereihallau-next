14cbf8b186813349cffd6182249668067cd28559
Marcel Weber <mrweber@gmx.ch>
2023-03-24 17:32:55 +0100

    Implemented mobile check to display respective navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
-import '@/styles/globals.css'
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import Navbar from "@/components/Navbar";
+import Navbar_Mobile from "@/components/Navbar_Mobile";
+import Footer from "@/components/Footer";
+import type { AppProps } from "next/app";
+import { useMediaQuery } from "@react-hook/media-query";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-  <Navbar />
-  <Component {...pageProps} />
-  <Footer />
-  </>
+  const isMobile = useMediaQuery("only screen and (max-aspect-ratio: 13/9)");
+
+  return (
+    <>
+      {!isMobile ? <Navbar /> : <Navbar_Mobile />}
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
