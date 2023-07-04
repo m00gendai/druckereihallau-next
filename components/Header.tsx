01b0f21acdf3927f6db4bc21f20fb8330e8faf67
Marcel Weber <mrweber@gmx.ch>
2023-07-04 03:14:34 +0200

    Added header images and favicon

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,16 +5,16 @@ export default function Header({title, content, url}:{title: string; content: st
         <Head>
             <title>{title}</title>
             <meta name="description" content={title} />
-            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:card" content="/logo.jpg" />
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={content} />
-            <meta name="twitter:image" content="" />
+            <meta name="twitter:image" content="/logo.jpg" />
             <meta property="og:type" content="article" />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={content} />
             <meta property="og:url" content={url} />
-            <meta property="og:image" content="" />
-            <link rel="icon" href="" />
+            <meta property="og:image" content="/logo.jpg" />
+            <link rel="icon" href="/logo.png" />
         </Head>
     )
 }
